d5a174066fca8d8ffea3d1496e91192241923571
Pierina Ormando <110559878+pierinaormando@users.noreply.github.com>
2023-10-16 23:21:36 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.addEventListener("DOMContentLoaded", function() {
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const generateExcuseButton = document.getElementById("generateExcuse");
+  //p que contiene el#excusa
+  const excuseParagraph = document.getElementById("excuse");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  function generateRandomExcuse() {
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+    return excuse;
+  }
+
+  generateExcuseButton.addEventListener("click", function() {
+    const randomExcuse = generateRandomExcuse();
+    excuseParagraph.innerHTML = randomExcuse;
+  });
+});
